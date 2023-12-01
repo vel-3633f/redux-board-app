0f2d63e20ea200d7e0ce8b1d67cc94cabb4bfbbe
vel-3633f <vel3633fu@gmail.com>
2023-12-01 15:40:54 +0900

    first

---
+++ src/redux/store.jsx
@@ -0,0 +1,14 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import counterReducer from "./counterSlice";
+
+import { configureStore } from "@reduxjs/toolkit";
+
+// export const store = configureStore({
+//   reducer: {
+//     counter: counterReducer,
+//   },
+// });
+
+export const store = configureStore({
+  
+})
